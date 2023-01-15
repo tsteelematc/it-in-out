8d7f85cfe79fa3045df19910ffd04ed690ecde65
tsteelematc <tsteele@madisoncollege.edu>
2023-01-15 13:55:53 -0600

    Experimenting with data for names...
    And image map areas...

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { ImageMap } from "@qiuz/react-image-map";
 import { Area } from '@qiuz/react-image-map';
+import data from './data.json';
+
 interface AreaType extends Area {
   href?: string;
 }
 
-let inArray: string[] = ["Tom"];
+let inArray: string[] = ["Tom1", "Kari"];
 
 const onMapClick = (area: AreaType, index: number) => {
   console.log(area);
@@ -28,7 +30,13 @@ const onMapClick = (area: AreaType, index: number) => {
 };
 
 const mapArea: any[] = [
-  {
+  {}
+  , {}
+  , {}
+  , {}
+  , {}
+  , {}
+  , {
     left: "2%",
     top: "3%",
     height: "14%",
@@ -47,7 +55,7 @@ const mapArea: any[] = [
         }}
       >
         <div>
-          Tom
+          {lookup.get("7")}
         </div>
         <div
           style={{
@@ -55,7 +63,40 @@ const mapArea: any[] = [
             , fontSize: "12px"
           }}
         >
-          {inArray.some(x => x == "Tom") ? "IN" : "OUT"}
+          {inArray.some(x => x == lookup.get("7")) ? "IN" : "OUT"}
+        </div>
+      </div>
+    ),
+    onMouseOver: () => console.log("map onMouseOver")
+  }
+  , {
+    left: "11.25%",
+    top: "3%",
+    height: "14%",
+    width: "9%",
+    style: { background: inArray.some(x => x == "Kari") ? "rgba(0, 255, 0, 0.10)": "rgba(127, 127, 127, 0.10)" },
+    render: (area: any, index: number) => (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          fontWeight: 'bold',
+        }}
+      >
+        <div>
+          {lookup.get("8")}
+        </div>
+        <div
+          style={{
+            fontWeight: "normal"
+            , fontSize: "12px"
+          }}
+        >
+          {inArray.some(x => x == lookup.get("8")) ? "IN" : "OUT"}
         </div>
       </div>
     ),
@@ -63,7 +104,12 @@ const mapArea: any[] = [
   }
 ];
 
+const lookup = new Map(Object.entries(data));
+
 function App() {
+  
+  console.log(lookup);
+
   const img = "https://tsteelematc.github.io/it-in-out/it-in-out-all.jpg";
 
   const ImageMapComponent = React.useMemo(
