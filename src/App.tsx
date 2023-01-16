09b015ae060a69ef802e40a53ba0a84e8b2fd7f3
tsteelematc <tsteele@madisoncollege.edu>
2023-01-15 19:27:03 -0600

    All mapped, meh...

--- src/App.tsx
+++ src/App.tsx
@@ -259,6 +259,306 @@ function App() {
         />
       ),  
     }
+    , {
+      left: "28.5%",
+      top: "34.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("11"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="11"
+        />
+      ),  
+    }  
+    , {
+      left: "37.5%",
+      top: "34.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("12"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="12"
+        />
+      ),  
+    }    
+    , {
+      left: "46.5%",
+      top: "34.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("13"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="13"
+        />
+      ),  
+    }    
+    , {
+      left: "61%",
+      top: "34.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("14"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="14"
+        />
+      ),  
+    }    
+    , {
+      left: "70%",
+      top: "34.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("15"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="15"
+        />
+      ),  
+    }    
+    , {
+      left: "79%",
+      top: "34.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("16"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="16"
+        />
+      ),  
+    }    
+    , {
+      left: "88%",
+      top: "34.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("17"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="17"
+        />
+      ),  
+    }    
+    , {
+      left: "88%",
+      top: "20.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("18"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="18"
+        />
+      ),  
+    }    
+    , {
+      left: "88.5%",
+      top: "3%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("19"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="19"
+        />
+      ),  
+    }    
+    , {
+      left: "79%",
+      top: "20.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("20"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="20"
+        />
+      ),  
+    }    
+    , {
+      left: "76.5%",
+      top: "3%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("21"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="21"
+        />
+      ),  
+    }   
+    , {
+      left: "70%",
+      top: "20.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("22"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="22"
+        />
+      ),  
+    }    
+    , {
+      left: "67.5%",
+      top: "3%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("23"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="23"
+        />
+      ),  
+    }   
+    , {
+      left: "61%",
+      top: "20.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("24"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="24"
+        />
+      ),  
+    }    
+    , {
+      left: "58.5%",
+      top: "3%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("25"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="25"
+        />
+      ),  
+    }   
+    , {
+      left: "46.5%",
+      top: "20.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("26"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="26"
+        />
+      ),  
+    }
+    , {
+      left: "46.5%",
+      top: "3%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("27"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="27"
+        />
+      ),  
+    }   
+    , {
+      left: "37.5%",
+      top: "20.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("28"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="28"
+        />
+      ),  
+    }
+    , {
+      left: "37.5%",
+      top: "3%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("29"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="29"
+        />
+      ),  
+    }   
+    , {
+      left: "28.5%",
+      top: "20.5%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("30"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="30"
+        />
+      ),  
+    }
+    , {
+      left: "28.5%",
+      top: "3%",
+      height: "14%",
+      width: "9%",
+      style: overlayBackground("31"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="31"
+        />
+      ),  
+    }   
+    , {
+      left: "90%",
+      top: "70%",
+      height: "13%",
+      width: "8%",
+      style: overlayBackground("32"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="32"
+        />
+      ),  
+    }   
+    , {
+      left: "90%",
+      top: "57%",
+      height: "13%",
+      width: "8%",
+      style: overlayBackground("33"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="33"
+        />
+      ),  
+    }   
+    , {
+      left: "73%",
+      top: "65%",
+      height: "12%",
+      width: "8%",
+      style: overlayBackground("34"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="34"
+        />
+      ),  
+    }   
+    , {
+      left: "74%",
+      top: "82%",
+      height: "13%",
+      width: "8%",
+      style: overlayBackground("35"),
+      render: (area: any, index: number) => (
+        <OverlayComponent 
+          index="35"
+        />
+      ),  
+    }   
   ];
   
   const onMapClick = async (area: AreaType, index: number) => {
